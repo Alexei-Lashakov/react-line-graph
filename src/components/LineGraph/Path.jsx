d485c3c47acf8ab8660468f2b02fb42e6f5b50f6
Alexei Lashakov <alexeilashakov@gmail.com>
2022-05-09 12:37:41 +0400

    add gradient

--- src/components/LineGraph/Path.jsx
+++ src/components/LineGraph/Path.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import types from '../_types';
+import React from "react";
+import types from "../_types";
 
 const Path = ({ accent, strokeWidth, path }) => (
-  <path fill="none" stroke={accent} strokeWidth={strokeWidth} d={path} />
+  <>
+    <defs>
+      <linearGradient id='grad1' x1={"0%"} x2={"50%"} x3={"100%"}>
+        <stop offset='0%' stop-color='#7e8dfb' stop-opacity='0.1' />
+        <stop offset='50%' stop-color='#7e8dfb' stop-opacity='0.25' />
+        <stop offset='100%' stop-color='#7e8dfb' stop-opacity='1' />
+      </linearGradient>
+    </defs>
+    <path
+      fill='transparent'
+      stroke='url(#grad1)'
+      strokeWidth={strokeWidth}
+      d={path}
+    />
+  </>
 );
 
 Path.propTypes = {
   accent: types.accent.isRequired,
   strokeWidth: types.strokeWidth.isRequired,
-  path: types.path.isRequired,
+  path: types.path.isRequired
 };
 
 export default Path;
